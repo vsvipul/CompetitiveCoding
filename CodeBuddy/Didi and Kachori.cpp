5a1ef0d694db0131ffab9c30c89914d7fc86dc24
ViPuL <vsvipul555@gmail.com>
2017-01-14 21:08:27 +0530

    Create Didi and Kachori.cpp

---
+++ CodeBuddy/Didi and Kachori.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int res=n%k;
+        cout<<res<<endl;
+    }
+    return 0;
+}
