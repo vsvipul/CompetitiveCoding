662ca0943638d3cf395afdeb8303399bdecca4af
ViPuL <vsvipul555@gmail.com>
2017-01-25 19:40:39 +0530

    Create Find Triplets.cpp

---
+++ CodeBuddy/Find Triplets.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x,y,z,i,temp;
+    long long int res=0;
+    vector <int> p,q,r;
+    cin>>x;
+    for (i=0;i<x;i++)
+    {
+    	cin>>temp;
+    	p.push_back(temp);
+    }
+    cin>>y;
+    for (i=0;i<y;i++)
+    {
+    	cin>>temp;
+    	q.push_back(temp);
+    }
+    cin>>z;
+    for (i=0;i<z;i++)
+    {
+    	cin>>temp;
+    	r.push_back(temp);
+    }
+    sort(p.begin(),p.end());
+    sort(q.begin(),q.end());
+    sort(r.begin(),r.end());
+    for (i=0;i<y;i++)
+    {
+    	vector <int>:: iterator it1=upper_bound(p.begin(),p.end(),q[i]);
+    	vector <int>:: iterator it2=lower_bound(r.begin(),r.end(),q[i]);
+    	res+=(it1-p.begin())*(r.end()-it2);
+    }
+    cout<<res<<endl;
+    return 0;
+}
