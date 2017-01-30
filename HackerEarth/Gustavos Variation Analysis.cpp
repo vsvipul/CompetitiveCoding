970681a21a845e3ae714ae86799c6b78bb68e966
ViPuL <vsvipul555@gmail.com>
2017-01-30 11:34:36 +0530

    Create Gustavos Variation Analysis.cpp

---
+++ HackerEarth/Gustavos Variation Analysis.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int t,n,i;
+    cin>>t;
+    while (t--)
+    {
+        cin>>n;
+        int arr[n];
+        for (i=0;i<n;i++)
+            cin>>arr[i];
+        sort(arr,arr+n);
+        cout<<arr[n-1]-arr[0]<<'\n';
+    }
+    return 0;
+}
