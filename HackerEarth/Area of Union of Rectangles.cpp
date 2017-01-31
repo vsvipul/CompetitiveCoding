6e5b41cdd1057246b19fd3b39f85c34ff70c2678
ViPuL <vsvipul555@gmail.com>
2017-01-31 14:39:15 +0530

    Create Area of Union of Rectangles.cpp

---
+++ HackerEarth/Area of Union of Rectangles.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int arr[1001][1001]={0};
+    int n,i,j,x1,x2,y1,y2,res=0;
+    cin>>n;
+    while (n--)
+    {
+        cin>>x1>>y1>>x2>>y2;
+        for (i=x1;i<x2;i++)
+        {
+            for (j=y1;j<y2;j++)
+            {
+                arr[i][j]=1;
+            }
+        }
+    }
+    for (i=0;i<1001;i++)
+    {
+        for (j=0;j<1001;j++)
+        {
+            if (arr[i][j]==1)
+                res++;
+        }
+    }
+    cout<<res<<'\n';
+    return 0;
+}
