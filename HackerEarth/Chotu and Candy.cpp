fb5483e72fd993a3063da87894e548a8d2f8a67c
ViPuL <vsvipul555@gmail.com>
2017-01-31 14:02:44 +0530

    Update and rename Tejus Cakewalk.cpp to Chotu and Candy.cpp

---
+++ HackerEarth/Chotu and Candy.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int t,n,c,i,min,temp;
+    cin>>t;
+    while (t--)
+    {
+        min=INT_MAX;
+        cin>>n>>c;
+        int arr[n];
+        for (i=0;i<n;i++)
+        {
+            cin>>arr[i];
+            temp=c-arr[i];
+            if (abs(temp)<min)
+                min=abs(temp);
+        }
+        cout<<min<<'\n';
+    }
+    return 0;
+}
