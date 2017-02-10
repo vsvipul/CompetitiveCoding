12a201572cf9dde4daa241e0ff6fee049041f992
ViPuL <vsvipul555@gmail.com>
2017-02-10 13:40:25 +0530

    Create Median of Numbers.cpp

---
+++ CodeBuddy/Median of Numbers.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    multiset <long long int> m;
+    multiset <long long int>::iterator it,it2;
+    int n,i;
+    cin>>n;
+    long long int temp;
+    double res;
+    cin>>temp;
+    m.insert(temp);
+    it=m.begin();
+    res=temp;
+    cout<<fixed<<setprecision(1)<<res<<'\n';
+    for (i=1;i<n;i++)
+    {
+        cin>>temp;
+        m.insert(temp);
+        if (i%2==0)
+        {
+            if (temp>=*it)
+                it++;
+        }
+        else
+        {
+            if (temp<*it)
+                it--;
+        }
+        if (i%2==0)
+        {
+            res=*it;
+        }
+        else
+        {
+            it2=it;
+            it2++;
+            res=(double) (*it+*it2)/2.0;
+        }
+        cout<<fixed<<setprecision(1)<<res<<'\n';
+    }
+    return 0;
+}
