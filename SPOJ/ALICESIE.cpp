5d9b7d6a70f83d9302368addc801f633f9396998
ViPuL <vsvipul555@gmail.com>
2017-02-13 17:13:03 +0530

    Create ALICESIE.cpp

---
+++ SPOJ/ALICESIE.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    int t,n;
+    cin>>t;
+    while (t--)
+    {
+    	cin>>n;
+    	cout<<(n+1)/2<<'\n';
+    }
+    return 0;
+}
