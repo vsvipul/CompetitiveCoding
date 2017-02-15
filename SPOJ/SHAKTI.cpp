27f470e93ea124e58cff959f2d143ebeb882cb8b
ViPuL <vsvipul555@gmail.com>
2017-02-15 07:20:35 +0530

    Create SHAKTI.cpp

---
+++ SPOJ/SHAKTI.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int n,t;
+    cin>>t;
+    while (t--)
+    {
+    	cin>>n;
+    	if (n%2==0)
+    	{
+    		cout<<"Thankyou Shaktiman\n";
+    	}
+    	else
+    	{
+    		cout<<"Sorry Shaktiman\n";
+    	}
+    }
+    return 0;
+}
