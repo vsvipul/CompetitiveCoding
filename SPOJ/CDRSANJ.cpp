77b4672a1b3341c5acc845a9822384fbca92bdaa
ViPuL <vsvipul555@gmail.com>
2017-03-10 21:27:04 +0530

    Update CDRSANJ.cpp

--- SPOJ/CDRSANJ.cpp
+++ SPOJ/CDRSANJ.cpp
@@ -18,7 +18,7 @@ void sieve()
 long long int fnc(int x)
 {
 	sieve();
-	if (x==0)	return 0;
+	if (x==0) return 0;
 	if (x==1) return 1;
 	if (x==2) return 2;
 	if (isprime[x])	return 0;
@@ -38,5 +38,5 @@ int main()
 	int x;
 	cin>>x;
 	cout<<fnc(x);
-  return 0;
+	return 0;
 }
