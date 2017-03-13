5aaea3e21713eac0e1463e903b5a95d1d90c35a7
ViPuL <vsvipul555@gmail.com>
2017-03-14 01:50:15 +0530

    Create NGM2.cpp

---
+++ SPOJ/NGM2.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    ll k,i,j,c;
+    ll n,res,tmp;
+    scanf("%lld%lld",&n,&k);
+    ll arr[k];
+    res=n;
+    for (i=0;i<k;i++)
+        scanf("%lld",&arr[i]);
+    for (i=0;i<(1<<k);i++)
+    {
+        tmp=1;c=0;
+        for (j=0;j<k;j++)
+        {
+            if(arr[j]==1)
+            {
+                res=0;
+                tmp=1;
+                break;
+            }
+            if (i&(1<<j))
+            {
+                if (((double) n/tmp)<((double) arr[j]/__gcd(arr[j],tmp)))
+                {
+                    tmp=1;
+                    break;
+                }
+                tmp*=arr[j]/__gcd(arr[j],tmp);;
+                c++;
+            }
+        }
+        if (tmp != 1)
+        {
+            if (c%2==1)
+                res-=n/tmp;
+            else
+                res+=n/tmp;
+        }
+    }
+    printf("%lld",res);
+    return 0;
+}
