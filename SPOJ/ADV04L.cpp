c830ad370bcd92a47af283d00f2638e3a3cb7f93
ViPuL <vsvipul555@gmail.com>
2017-03-16 09:47:52 +0530

    Create ADV04L.cpp

---
+++ SPOJ/ADV04L.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    vector <ll> v;
+    vector <ll>::iterator it;
+    int i,t;
+	v.push_back(1);
+    v.push_back(1);
+    for (i=2;i<90;i++)
+    {
+    	v.push_back(v[i-1]+v[i-2]);
+    }
+    ll m,tmp,res;
+    cin>>t;
+    while (t--)
+    {
+    	res=0;
+    	cin>>m;
+    	tmp=m;
+    	while (tmp>0)
+    	{
+    		it=upper_bound(v.begin(),v.end(),tmp);
+    		res+=*it;
+    		if (it!=v.begin())
+    			it--;
+    		tmp-=*it;
+    	}
+    	cout<<res<<'\n';
+    }
+    return 0;
+}
