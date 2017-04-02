2ab504c6f9c15f00f37ae8f7a448350a758c418d
ViPuL <vsvipul555@gmail.com>
2017-04-03 00:27:12 +0530

    Create 3 Idiots.cpp

---
+++ CodeBuddy/3 Idiots.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,i,j;
+    string a,b;
+    cin>>t;
+    while (t--)
+    {
+        j=0;
+        cin>>a>>b;
+        for (i=0;i<a.length();i++)
+        {
+            if (a[i]==b[j])
+                j++;
+        }
+        if (j==b.length())
+            cout<<"Yes\n";
+        else
+            cout<<"No\n";
+    }
+    return 0;
+}
