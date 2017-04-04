90196e1a75ee268000306b82f7128e6925eaef08
ViPuL <vsvipul555@gmail.com>
2017-04-04 10:29:08 +0530

    Update Finding Nemo.cpp

--- CodeBuddy/Finding Nemo.cpp
+++ CodeBuddy/Finding Nemo.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
+#define ll long long int
 using namespace std;
 
 int main()
 {
-    int t,n,x,y;
-    cin>>t;
-    while (t--)
-    {
-        cin>>n;
-        y=n/65536;
-        x=n-y*65536;
-        cout<<x<<" "<<y<<"\n";
-    }
-    return 0;
+	int t;
+	ll n,x,y;
+	cin>>t;
+	while (t--)
+	{
+		cin>>n;
+		y=n/65536;
+		x=n-y*65536;
+		cout<<x<<" "<<y<<"\n";
+	}
 }
