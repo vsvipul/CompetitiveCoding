8e862c9cbc58150f3c7aa8b6731b2998e416c696
ViPuL <vsvipul555@gmail.com>
2017-06-16 23:54:45 +0530

    Update Dangal Arena.cpp

--- CodeBuddy/Dangal Arena.cpp
+++ CodeBuddy/Dangal Arena.cpp
@@ -1,29 +1,59 @@
 #include <bits/stdc++.h>
+#define mod 1000000007
+#define ll long long int
 using namespace std;
 
-double fnc(int n, double x[], double y[])
+int isprime[1000001];
+
+void sieve()
 {
-    double area=0.0;
-    int i,j=n-1;
-    for (i=0;i<n;i++)
-    {
-        area+=(x[j]+x[i])*(y[j]-y[i]);
-        j=i;
-    }
-    return abs(area/2.0);
+	int i,j;
+	memset(isprime,1,sizeof(isprime));
+	for (i=2;i<1000001;i++)
+	{
+		if (isprime[i])
+		{
+			for (j=2*i;j<1000001;j+=i)
+				isprime[j]=0;
+		}
+	}
+}
+
+int fnc(int n)
+{
+	ll i,res,count,tmp,x;
+	res=1;
+	for (i=2;i<=n;i++)
+	{
+		if (isprime[i])
+		{
+			count=0;
+			x=n;
+			x/=i;
+			while (x!=0)
+			{
+				x/=i;
+				count++;
+			}
+			tmp=1;
+			while (count--)
+				tmp*=i;
+			res=((res%mod)*(tmp%mod))%mod;
+		}
+	} 
+	return res;
 }
 
 int main()
 {
-    int t,n,i;
-    cin>>t;
-    while (t--)
-    {
-        cin>>n;
-        double x[n],y[n];
-        for (i=0;i<n;i++)
-            cin>>x[i]>>y[i];
-        cout<<fixed<<setprecision(1)<<fnc(n,x,y)<<endl;
-    }
-    return 0;
+    ios_base::sync_with_stdio(false);
+    int t,n;
+	scanf("%d",&t);
+	sieve();
+	while (t--)
+	{
+		scanf("%d",&n);
+	    printf("%d\n",fnc(n));
+	}
+  return 0;
 }
