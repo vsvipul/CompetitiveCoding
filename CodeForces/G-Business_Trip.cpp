fb726d3f2cd216de35d6aae4905d01a37dd61153
ViPuL <vsvipul555@gmail.com>
2018-07-23 19:11:49 +0530

    Create G-Business_Trip.cpp

---
+++ CodeForces/G-Business_Trip.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define vi vector <int>
+#define pb push_back
+#define ll long long int
+#define vl vector <ll>
+#define fi cin.tie(NULL);ios_base::sync_with_stdio(false)
+#define T int t; cin>>t; while(t--)
+#define s(n) scanf("%d",&n)
+#define s2(a,b) scanf("%d%d",&a,&b)
+#define s3(a,b,c) scanf("%d%d%d",&a,&b,&c)
+ll power(ll a, ll n) {ll p = 1;while (n > 0) {if(n%2) {p = p * a;} n >>= 1; a *= a;} return p;}
+ll power(ll a, ll n, ll mod) {ll p = 1;while (n > 0) {if(n%2) {p = p * a; p %= mod;} n >>= 1; a *= a; a %= mod;} return p % mod;}
+using namespace std;
+
+int main()
+{
+	fi;
+	int n,i,tmp,sum=0,res=0;
+	cin>>n;
+	vi v;
+	for (i=0;i<12;i++)
+	{
+		cin>>tmp;
+		v.pb(tmp);
+	}
+	sort(v.begin(),v.end(),greater<int>());
+	for (i=0;i<12;i++)
+	{
+		if (sum>=n)
+		{
+			cout<<res;
+			break;
+		}
+		sum+=v[i];
+		res++;
+		if (sum>=n)
+		{
+			cout<<res;
+			break;
+		}
+	}
+	if (sum<n)
+		cout<<"-1";
+    return 0;
+}
