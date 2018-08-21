58401378d2f70627edf6f517f1668330b38df256
ViPuL <vsvipul555@gmail.com>
2018-08-21 19:38:48 +0530

    Create A-Palindromic_Twist.cpp

---
+++ CodeForces/A-Palindromic_Twist.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define vi vector <int>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define vll vector <ll>
+#define vi vector <int>
+#define pii pair<int,int>
+#define all(a) (a).begin(),(a).end()
+#define fi cin.tie(NULL);ios_base::sync_with_stdio(false)
+#define T int t; cin>>t; while(t--)
+#define F first
+#define S second
+#define sz(x) (int)x.size()
+#define endl '\n';
+#define rep(i,a,b) for (int i=a;i<b;i++)
+
+ll power(ll a, ll n) {ll p = 1;while (n > 0) {if(n%2) {p = p * a;} n >>= 1; a *= a;} return p;}
+ll power(ll a, ll n, ll mod) {ll p = 1;while (n > 0) {if(n%2) {p = p * a; p %= mod;} n >>= 1; a *= a; a %= mod;} return p % mod;}
+using namespace std;
+
+bool check(string str)
+{
+	int n=str.length(),i=0;
+	int j=n-1;
+	while (i<j)
+	{
+		if (!(((abs(int(str[i]-str[j])))==2)||(abs(int(str[i]-str[j])))==0))
+			return false;
+		i++;j--;
+	}
+	return true;
+}
+
+int main()
+{
+	fi;
+	T
+	{
+		int n;
+		cin>>n;
+		string str;
+		cin>>str;
+		if (check(str))
+			cout<<"YES\n";
+		else
+			cout<<"NO\n";
+	}
+    return 0;
+}
