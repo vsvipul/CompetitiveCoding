2ae3ba72c2d24a034f2207d28bf2326fd618680f
ViPuL <vsvipul555@gmail.com>
2018-08-23 18:46:30 +0530

    Create 71A-Way_Too_Long_Words.cpp

---
+++ CodeForces/71A-Way_Too_Long_Words.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define vi vector <int>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define vll vector <ll>
+#define vi vector <int>
+#define ii pair<int,int>
+#define all(a) (a).begin(),(a).end()
+#define fi cin.tie(NULL);ios_base::sync_with_stdio(false)
+#define T int t; cin>>t; while(t--)
+#define F first
+#define S second
+#define sz(x) (int)x.size()
+#define endl '\n';
+#define rep(i,a,b) for (int i=a;i<b;i++)
+
+ll power(ll a, ll n) {ll p = 1;while (n > 0) {if(n%2) {p = p * a;} n >>= 1; a *= a;} return p;}
+ll power(ll a, ll n, ll mod) {ll p = 1;while (n > 0) {if(n%2) {p = p * a; p %= mod;} n >>= 1; a *= a; a %= mod;} return p % mod;}
+using namespace std;
+
+int main()
+{
+	fi;
+	T
+	{
+		string str;
+		cin>>str;
+		if (str.length()>10)
+			cout<<str[0]<<str.length()-2<<str[str.length()-1]<<"\n";
+		else
+			cout<<str<<"\n";
+	}
+    return 0;
+}
