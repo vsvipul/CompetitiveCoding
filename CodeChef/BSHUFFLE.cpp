1185aad9d4a3b12a3312a2b91c689b3058f21be6
ViPuL <vsvipul555@gmail.com>
2018-09-18 03:27:11 +0530

    Create BSHUFFLE.cpp

---
+++ CodeChef/BSHUFFLE.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define vi vector <int>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define vll vector <ll>
+#define vi vector <int>
+#define ii pair<int,int>
+#define all(a) (a).begin(),(a).end()
+#define fi cin.tie(NULL);ios_base::sync_with_stdio(false)
+#define T int t; cin>>t; while(t--)
+#define F first
+#define S second
+#define sz(x) (int)x.size()
+#define endl '\n';
+#define rep(i,a,b) for (int i=a;i<b;i++)
+
+ll power(ll a, ll n) {ll p = 1;while (n > 0) {if(n%2) {p = p * a;} n >>= 1; a *= a;} return p;}
+ll power(ll a, ll n, ll mod) {ll p = 1;while (n > 0) {if(n%2) {p = p * a; p %= mod;} n >>= 1; a *= a; a %= mod;} return p % mod;}
+using namespace std;
+
+int main()
+{
+	fi;
+	int n,i;
+	cin>>n;
+	for (i=2;i<=n/2;i++)
+		cout<<i<<" ";
+	cout<<1<<" ";
+	for (i=(n/2)+2;i<=n;i++)
+		cout<<i<<" ";
+	cout<<(n/2)+1<<"\n";
+	cout<<n<<" ";
+	for (i=1;i<=n-1;i++)
+		cout<<i<<" ";
+	cout<<"\n";
+    return 0;
+}
