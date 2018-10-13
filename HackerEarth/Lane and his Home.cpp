3127586e045f153a46de94627dbc339f32ac2ab9
ViPuL <vsvipul555@gmail.com>
2018-10-13 17:32:39 +0530

    Create Lane and his Home.cpp

---
+++ HackerEarth/Lane and his Home.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+ 
+#define vi vector <int>
+#define mii map <int,int>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define vll vector <ll>
+#define vi vector <int>
+#define ii pair<int,int>
+#define all(a) (a).begin(),(a).end()
+#define FI cin.tie(NULL);ios_base::sync_with_stdio(false)
+#define T int t; cin>>t; while(t--)
+#define F first
+#define S second
+#define sz(x) (int)x.size()
+#define endl '\n';
+#define rep(i,a,b) for (int i=a;i<b;i++)
+ 
+ll power(ll a, ll n) {ll p = 1;while (n > 0) {if(n%2) {p = p * a;} n >>= 1; a *= a;} return p;}
+ll power(ll a, ll n, ll mod) {ll p = 1;while (n > 0) {if(n%2) {p = p * a; p %= mod;} n >>= 1; a *= a; a %= mod;} return p % mod;}
+using namespace std;
+ 
+int main()
+{
+    FI;
+    int t,a,b,s,sum;
+    cin>>t;
+    while (t--)
+    {
+		cin>>a>>b>>s;
+		sum=abs(a)+abs(b);
+		if ((!((sum%2) ^ (s%2)) && (s>=sum)))
+			cout<<"Yes\n";
+		else
+			cout<<"No\n";
+ 
+	}
+    return 0;
+}
